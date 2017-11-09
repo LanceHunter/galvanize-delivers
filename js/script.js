b179630140deaae45c9fe37efc599b5826af86b5
Lance Hunter <lancehunter@gmail.com>
2017-11-09 15:18:07 -0600

    Started work on Javascript.

---
+++ js/script.js
@@ -0,0 +1,19 @@
+(function() {
+
+console.log("This works");
+
+//Here's where we get the customer data and order data divs.
+let orderData = $('#orderData');
+let customerData = $('customerData');
+
+//Here's where we get the links for each of the order types.
+let orderBurger = $('#orderBurger');
+let orderPizza = $('#orderPizza');
+let orderRibs = $('#orderRibs');
+let orderIceCream = $('#orderIceCream');
+
+let orderButton = $('#orderButton');
+
+
+
+})();
